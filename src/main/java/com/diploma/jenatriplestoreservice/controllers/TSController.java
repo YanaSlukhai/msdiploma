25eae4fde8a3750f41aee62bd5c2ed2d3c101f51
Yana_Slukhai <Yana_Slukhai@epam.com>
2018-04-19 17:12:08 +0300

    refactoring

--- src/main/java/com/diploma/jenatriplestoreservice/controllers/TSController.java
+++ src/main/java/com/diploma/jenatriplestoreservice/controllers/TSController.java
@@ -1,7 +1,8 @@
 package com.diploma.jenatriplestoreservice.controllers;
 
-import com.diploma.jenatriplestoreservice.repositories.DataSetCreator;
+import com.diploma.jenatriplestoreservice.repositories.TSRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.jena.ontology.OntProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,12 @@ import java.util.List;
 @Controller
 public class TSController {
     @Autowired
-    DataSetCreator dataSetCreator;
+    TSRepository dataSetCreator;
 
     @GetMapping(value = "/getdata/get-all-properties")
     @ResponseBody
     public String getAllProperties() {
-        List<String> allOntProperties = dataSetCreator.getAllOntProperties();
+        List<OntProperty> allOntProperties = dataSetCreator.getAllOntProperties();
         final StringWriter sw =new StringWriter();
         final ObjectMapper mapper = new ObjectMapper();
         try {
